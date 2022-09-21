ecec0229a7ceac9b054d2fa546ac23fc08af4600
dake2007 <pdskill@163.com>
2022-09-22 03:40:20 +0800

    提交

--- assets/js/login.js
+++ assets/js/login.js
@@ -58,9 +58,11 @@ $(function () {
       password: $("#form_reg [name=password]").val(),
     };
     $.post("/api/register", data, function (res) {
+      // console.log(res);
       if (res.status !== 0) {
         return layer.msg(res.message);
       }
+
       layer.msg("注册成功，请登录！");
       // 模拟人的点击行为
       $("#link_login").click();
@@ -78,8 +80,10 @@ $(function () {
       data: $(this).serialize(),
       success: function (res) {
         if (res.status !== 0) {
+          console.log(res);
           return layer.msg("登录失败！");
         }
+
         layer.msg("登录成功！");
         // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem("token", res.token);

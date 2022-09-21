7fc83699d08930d647b6f487fa83e8b85bb8f5ab
dake2007 <pdskill@163.com>
2022-09-21 15:50:39 +0800

    提交一下

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,9 @@
 $.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = "http://127.0.0.1:3007" + options.url;
+  if (options.url.indexOf("/my") !== -1)
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
 });
+//统一为有权限的接口，设置header请求头
